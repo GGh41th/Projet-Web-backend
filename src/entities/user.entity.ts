4223f287a18f2edd1b0f5a03050a303f40e3c34a
Ghaith <gtari20ghaith33@gmail.com>
2026-01-14 19:50:00 +0100

    added user entity

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,63 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Index,
+  BeforeInsert,
+  BeforeUpdate,
+} from 'typeorm';
+import * as bcrypt from 'bcrypt';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  @Index()
+  email: string;
+
+  @Column({ unique: true })
+  @Index()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column({ nullable: true })
+  name: string;
+
+  @Column({ nullable: true })
+  lastName: string;
+
+  @Column({ type: 'text', nullable: true })
+  bio: string;
+
+  @Column({ 
+    type: 'enum', 
+    enum: ['user', 'admin'], 
+    default: 'user' 
+  })
+  role: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword() {
+    if (this.password && !this.password.startsWith('$2b$')) {
+      // Only hash if password is not already hashed
+      this.password = await bcrypt.hash(this.password, 10);
+    }
+  }
+
+  async validatePassword(password: string): Promise<boolean> {
+    return bcrypt.compare(password, this.password);
+  }
+}
